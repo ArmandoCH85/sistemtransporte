cfa4a77a53c6631397abe341bd01d02d44eeb682
Armando <armando.correah@gmail.com>
2025-06-09 22:36:55 -0500

    No obligatorio

---
+++ storage/framework/views/a82571a21bf1b79587c8fed1d9e4eb5c.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body style="margin: 0; padding: 0; box-sizing: border-box; font-family: Arial, Helvetica, sans-serif;">
+    <?php echo $data['content']; ?>
+
+</body>
+</html>
+<?php /**PATH C:\Users\arman\Herd\transporte\resources\views/emails/test.blade.php ENDPATH**/ ?>
